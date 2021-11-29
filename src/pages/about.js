be95d9987375028a3e5928c04fea97cb87314a30
eloicasamayor <37459666+eloicasamayor@users.noreply.github.com>
2021-11-29 21:56:30 +0100

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/Layout"
+import styled from "styled-components"
+
+const about = () => {
+  return (
+    <Layout>
+      <h1>About page</h1>
+    </Layout>
+  )
+}
+
+export default about
