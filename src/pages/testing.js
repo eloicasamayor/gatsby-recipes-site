4e7cc929e89d2951e14e9f9ddb889cba77577deb
eloicasamayor <37459666+eloicasamayor@users.noreply.github.com>
2021-12-06 20:11:42 +0100

    learning gatsby-images-plugin

--- src/pages/testing.js
+++ src/pages/testing.js
@@ -1,33 +1,15 @@
 import React from "react"
 import { graphql } from "gatsby"
-
-const Testing = ({ data }) => {
-  const author = data.site.info.author
+import Layout from "../components/Layout"
+import Gallery from "../examples/Gallery"
+const Testing = ({}) => {
   return (
-    <div>
-      <h2>author: {author}</h2>
-    </div>
+    <Layout>
+      <main className="page">
+        <Gallery />
+      </main>
+    </Layout>
   )
 }
 
-export const data = graphql`
-  query {
-    site {
-      info: siteMetadata {
-        complexData {
-          name
-          age
-        }
-        person {
-          name
-          age
-        }
-        title
-        simpleData
-        description
-        author
-      }
-    }
-  }
-`
 export default Testing
